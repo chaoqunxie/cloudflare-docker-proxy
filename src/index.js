30177a48d7b95bb98c05df875827095b1307ff00
chaoqunxie <44899524+chaoqunxie@users.noreply.github.com>
2024-06-23 22:12:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.sharemoon.club": dockerHub,
+  "quay.sharemoon.club": "https://quay.io",
+  "gcr.sharemoon.club": "https://gcr.io",
+  "k8s-gcr.sharemoon.club": "https://k8s.gcr.io",
+  "k8s.sharemoon.club": "https://registry.k8s.io",
+  "ghcr.sharemoon.club": "https://ghcr.io",
+  "cloudsmith.sharemoon.club": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.sharemoon.club": dockerHub,
 };
 
 function routeByHosts(host) {
